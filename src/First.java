647d05158376baed01796793f9ccdfa768948941
Alex <aleaute@epsi.fr>
2011-12-09 12:19:58 +0100

    coube du projet

--- src/First.java
+++ src/First.java
@@ -12,21 +12,26 @@ public class First {
     
 	//create a dataset...
 	String c1 = "courbe 1", c2 = "courbe 2";
-	double values1[][] = new double[2][50];
-	double values2[][] = new double[2][50];
+	double values1[][] = new double[2][100];
+	double values2[][] = new double[2][100];
 	DefaultXYDataset data=new DefaultXYDataset();
 
-	Double x = 0.0;
-	for( int i = 0 ; i < 50; i++,x+=0.1)
+	Double x = -5.0;
+	for( int i = 0 ; i < 100; i++,x+=0.1)
 	{
-		values1[0][i] = x;
-		values1[1][i] = Math.cos(x);
-			
-		values2[0][i] = x;
-		values2[1][i] = Math.sin(x);
+		if(i < 50)
+		{
+			values1[0][i] = x;
+			values1[1][i] = Math.exp(-Math.pow(x, 2));
+		}
+		else
+		{
+			values1[0][i] = x;
+			values1[1][i] = Math.cos(x);
+		}
 	}
 	data.addSeries(c1, values1);
-	data.addSeries(c2, values2);
+	//data.addSeries(c2, values2);
  
     //create a chart...
 	JFreeChart chart=ChartFactory.createXYLineChart("XYLineChart", "x", "y", data, PlotOrientation.VERTICAL, true, false, false);

198221c8d456884536dcfe184818668fd6500ce6
Alex <aleaute@epsi.fr>
2011-12-09 10:36:44 +0100

    tracage courbe

--- src/First.java
+++ src/First.java
@@ -2,32 +2,34 @@ import  org.jfree.chart.ChartFactory;
 import  org.jfree.chart.ChartFrame;
 import  org.jfree.chart.JFreeChart;
 import  org.jfree.data.category.DefaultCategoryDataset;
+import org.jfree.data.xy.DefaultXYDataset;
 import org.jfree.chart.plot.PlotOrientation;
 
 
 public class First {
  
-  public static void main(String[] args) {
+	public static void main(String[] args) {
     
-  //create a dataset...
-	  Double c = 1.0, c1 = 2.0, y = 0.0;
-	DefaultCategoryDataset data=new DefaultCategoryDataset();
+	//create a dataset...
+	String c1 = "courbe 1", c2 = "courbe 2";
+	double values1[][] = new double[2][50];
+	double values2[][] = new double[2][50];
+	DefaultXYDataset data=new DefaultXYDataset();
 
-	for( Double x = 0.0; x < 5; x+=0.1)
+	Double x = 0.0;
+	for( int i = 0 ; i < 50; i++,x+=0.1)
 	{
-		y = Math.cos(x);
-		data.addValue(y,c,x);
+		values1[0][i] = x;
+		values1[1][i] = Math.cos(x);
+			
+		values2[0][i] = x;
+		values2[1][i] = Math.sin(x);
 	}
-    
-	for( Double x = 0.0; x < 5; x+=0.1)
-	{
-		y = Math.sin(x);
-		data.addValue(y,c1,x);
-	}
-
+	data.addSeries(c1, values1);
+	data.addSeries(c2, values2);
  
     //create a chart...
-    JFreeChart chart=ChartFactory.createLineChart("lineChart", "x", "y", data, PlotOrientation.VERTICAL, false, false, false);
+	JFreeChart chart=ChartFactory.createXYLineChart("XYLineChart", "x", "y", data, PlotOrientation.VERTICAL, true, false, false);
    
     //create and display a frame...
     ChartFrame frame=new ChartFrame("First",chart);
